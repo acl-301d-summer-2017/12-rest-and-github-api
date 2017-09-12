c9e8732d4dac9d7401903d2fe7b2db0145a325c3
CHRISTINA MILLS <CHRISTINA.A.MILLS@GMAIL.COM>
2017-09-12 14:32:14 -0700

    did first 3 TODOs

--- starter-code/public/scripts/models/repo.js
+++ starter-code/public/scripts/models/repo.js
@@ -7,12 +7,21 @@ var app = app || {};
   repos.all = [];
 
   repos.requestRepos = function(callback) {
+    $.ajax( {
+      url: 'http://api.github.com/users/christinamills/repos',
+      method: 'GET',
+      headers: {
+        'Authorization': `token ${token}`
+      }
+    }).then(data => {
+      console.log('got the data');
+    }).then(callback)
     // TODO: How would you like to fetch your repos? Don't forget to call the callback.
     //       Remember that the callback function we'll want to call relies on repos.all
     //       being an array with a bunch of repo objects in it, so you'll need to
     //       populate it with the response from Github before you call the callback.
 
-  };
+  }
 
   // REVIEW: Model method that filters the full collection for repos with a particular attribute.
   // You could use this to filter all repos that have a non-zero `forks_count`, `stargazers_count`, or `watchers_count`.
