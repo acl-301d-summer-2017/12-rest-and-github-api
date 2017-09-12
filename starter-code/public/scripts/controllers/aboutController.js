d31eccc79e70b80685774137167f8d251d3fb3a5
Jennifer Elton <justjenny40@hotmail.com>
2017-09-12 16:33:47 -0700

    killed it

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -5,12 +5,12 @@ var app = app || {};
   const aboutController = {};
 
   aboutController.index = () => {
-    $('#about').show().siblings().hide(); // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
-
-    // TODO: Call a function to load all the data.
-    // Pass a view function as a callback, so the view will render after the data is loaded.
-
+    $('#about').show().siblings().hide(); 
+    app.repos.requestRepos(app.repoView.index);
   };
+  // REVIEW: We have a slight refactor in selectors here, which has reduced the amount of code from the last lab.
+  // DONE:TODO: Call a function to load all the data.
+  // Pass a view function as a callback, so the view will render after the data is loaded.
 
   module.aboutController = aboutController;
 })(app);
